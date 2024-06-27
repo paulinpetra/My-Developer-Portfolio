14461692923bb6599c55dbd7d92f3e9348cddf04
Petra Paulin <petra.oster.paulin@gmail.com>
2024-06-27 15:49:11 +0200

    Hero done

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,5 +1,8 @@
 import { Spotlight } from "./ui/Spotlight";
 import { TextGenerateEffect } from "./ui/TextGenerateEffect";
+import MagicButton from "./ui/MagicButton";
+import { FaLocationArrow } from "react-icons/fa6";
+
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
@@ -21,16 +24,24 @@ const Hero = () => {
       <div className="flex justify-center relative my-20 z-10">
         <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
           <h2 className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
-            Dynamic Web Magic with Next.js
+            Bringing Visionary Concepts to Life
           </h2>
           <TextGenerateEffect
             className="capitalize text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Transforming concepts into seamless experiences"
+            words="Turning ideas into functional applications "
           />
-          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+          <p className="capitalize text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             {" "}
-            Hi, I&apos;m Petra, a Next.js Developer based in Sweden
+            Hi, I&apos;m Petra, a Software Developer with React Expertise based
+            in Sweden
           </p>
+          <a href="#about">
+            <MagicButton
+              title="Show my work"
+              icon={<FaLocationArrow />}
+              position="right"
+            />
+          </a>
         </div>
       </div>
     </div>
