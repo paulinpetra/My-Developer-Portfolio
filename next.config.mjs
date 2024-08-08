7d88675403cba17666005857442401c53398b933
Petra Paulin <petra.oster.paulin@gmail.com>
2024-08-08 11:54:34 +0200

    remove es lint for builds

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  eslint: {
+    // Ignore ESLint errors during builds
+    ignoreDuringBuilds: true,
+  },
+};
 
 export default nextConfig;
